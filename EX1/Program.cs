8e7e19373f0e17e4bf5ce3872dc8d84766194fab
Mihail Mencyk <m.mencyk@gmail.com>
2022-12-21 00:27:45 +0300

    Size array2

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,3 +1,10 @@
 ﻿string[] array1 = new string[] { "1995", "happy", "lol", "gb33", "leo", "-23333", "59888" };
 string s1 = string.Join(", ", array1);
 System.Console.WriteLine($"[{s1}]");
+
+int size = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3) size++;
+}
+System.Console.WriteLine(size);
